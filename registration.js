67db35517790f9b5ec1f6280afbe8fc3cb792180
amey bhattad <amey.bhattad72@gmail.com>
2022-12-14 19:24:40 +0530

    minor changes

--- registration.js
+++ registration.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const client = require("./database");
 
+/* Add data to postgres*/
 router.post("/register",async (req,res)=>{
     try {
         console.log(req.body);
